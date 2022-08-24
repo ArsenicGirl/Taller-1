e2f5ccc1c9f44416f9c0e79c3c2ec02f7759c0ab
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2022-08-24 12:50:23 -0500

    Add files via upload

---
+++ Ejercicios Sarita Salazar/punto 1/punto1.js
@@ -0,0 +1,28 @@
+'use strict'
+//Realizar un programa que pida al usuario 2 números y se realicen las 4operaciones básicas (Suma, resta, multiplicación, división). Imprimir los resultados.
+let num1;
+let num2;
+let resta;
+let multiplicacion;
+let division;
+
+resta = 0;
+multiplicacion = 0;
+division = 0;
+
+num1 = prompt("ingrese un número: ");
+num2 = prompt("ingrese otro segundo número: ");
+
+    function suma (num1= 0, num2 = 0){
+        return num1 + num2;
+    }
+    let result = 0;
+    result = suma (3,5);
+    console.log(result);
+    
+function resta (num1= 0, num2 = 0){
+            return num1 + num2;
+        }
+        let resultado = 0;
+        result = resta (3,5);
+        console.log(resultado);
